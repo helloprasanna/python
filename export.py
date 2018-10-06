b1d422092bee6d851d01feb3e21cb0c7fd5add50
Prasanna <prasanna-mac@prasanna-mac.com>
2018-10-06 11:23:47 +0100

    Git checkin
    first checkin

---
+++ export.py
@@ -0,0 +1,36 @@
+
+# coding: utf-8
+
+# In[2]:
+
+
+a = 3
+b = 1
+
+print(a)
+print(a+b)
+
+
+# In[3]:
+
+
+c="hello "+str(a+b)
+print(c)
+print(len(c))
+
+
+# In[22]:
+
+
+for i in range(2,5):
+    print(i)
+
+
+# In[17]:
+
+
+string = 'string'
+for i in range(11):
+    string += str(i)
+print (string)
+
